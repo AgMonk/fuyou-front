2318eb7b3d9bd48053a4259ae02eb862946f0d9c
ginstone <hjg719@139.com>
2021-07-09 12:57:53 +0800

    导航组件

---
+++ src/components/my-navigation-item.vue
@@ -0,0 +1,38 @@
+<!--suppress HtmlUnknownTag -->
+<template>
+  <el-submenu v-if="route.children" :index="route.path">
+    <template #title>{{ route.name }}</template>
+    <my-navigation-item v-for="(child,i) in route.children" :key="i" :route="child" :parent-path="path"/>
+  </el-submenu>
+  <el-menu-item v-else :index="path" :route="route">
+    {{ route.name }}
+  </el-menu-item>
+</template>
+
+<script>
+export default {
+  name: "my-navigation-item",
+  data() {
+    return {
+      path:this.parentPath+(this.route.path.startsWith(`/`)?``:`/`)+this.route.path
+    }
+  },
+  methods: {},
+  mounted() {
+    console.log(this.path)
+  },
+  props: {
+    route: {
+      required: true
+    },
+    parentPath: {
+      type: String,
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/my-navigation.vue
@@ -0,0 +1,44 @@
+<!--suppress HtmlUnknownTag -->
+<template>
+  <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      @select="select"
+      router
+      active-text-color="#ffd04b">
+  <my-navigation-item v-for="(route,i) in routes" :key="i" :route="route" parent-path="" />
+  </el-menu>
+</template>
+
+<script>
+import {routes} from "@/router";
+import MyNavigationItem from "@/components/my-navigation-item";
+
+export default {
+  name: "my-navigation",
+  components: {MyNavigationItem},
+  data() {
+    return {
+      routes,
+      activeIndex:undefined,
+    }
+  },
+  methods: {
+    select(index,indexPath){
+      console.log(index)
+      console.log(indexPath)
+    }
+  },
+  mounted() {
+    console.log(routes)
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
